a28d7eb46f155837fa283a71ce8e41079265df04
melli <melchiorg@gmail.com>
2021-01-01 20:36:08 +0100

    7Refactor2: Game.isOver() code only once.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -7,10 +7,10 @@ class Game {
 
     fun roll(points :Int) {
         if (points<0||points>10) throw IllegalArgumentException("Cannot roll fewer than 0 points or more than 10 points.")
-        if (rounds>=20) throw IllegalStateException("Cannot roll more than 20 times per Game.")
+        if (isOver()) throw IllegalStateException("Cannot roll more than 20 times per Game.")
         this.points += points
         rounds++
     }
 
-    fun isGameOver() = rounds >= 20
+    fun isOver() = rounds >= 20
 }
