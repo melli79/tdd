d8421297295b164f0b9976efa28aad9bf5bd0b20
melli <melchiorg@gmail.com>
2021-01-01 18:23:33 +0100

    2Red: rolling the ball -- test fails.

--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -1,10 +1,17 @@
 
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 object GameTest {
     @Test fun createGame() {
         val game = Game()
         assertNotNull(game)
+    }
+
+    @Test fun roll() {
+        val game = Game()
+        game.roll(0)
+        val result = game.points
+        assertEquals(0, result)
     }
 }
